de5a2a2da1085b9e4e708af006a582bdbc9de213
retajal <retaj.shehri@outlook.com>
2024-02-18 22:12:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("hello ")
+friends = int(input("how many friends do you ?"))
+sweets = int(input("how many sweets do you have ?"))
+share = friends / sweets
+how_many_left = friends % sweets
+print("give youe friends " , share, "The rest of the sweets ",how_many_left )
+print("have a nice day ")
